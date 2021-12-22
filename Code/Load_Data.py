2cfef43f7a3e780123a9b6a180a77f4026ad425d
work-213.x0p5l@simplelogin.co <YAk8U$L2EjUjvXofz@QPf@o9JY3tg9oxTaX7fTV%353BGSg7E4PmR8vLaduoQ!N2urKLz>
2021-12-22 20:25:30 +0100

    modified:   Code/Load_Data.py 	modified:   Code/Zero_Objective_Func.py 	new file:   Code/__pycache__/Load_Data.cpython-38.pyc 	new file:   Results/L1_norm.txt 	new file:   Results/ZeroObjectiveFunc.txt

--- Code/Load_Data.py
+++ Code/Load_Data.py
@@ -1,8 +1,14 @@
 from scipy.io import loadmat
+from pathlib import Path
+root = Path(__file__).parent.parent
 
-vars = loadmat('sp_ln_sp_data.mat')
-n = vars['n']
-N = vars['N']
-M = vars['M']
-X = vars['X']
-Y = vars['Y']
+def loadvars():
+    vars = loadmat(root.as_posix() +"/"+ "Data" +"/" + 'sp_ln_sp_data.mat')
+    n = vars['n']
+    N = vars['N']
+    M = vars['M']
+    X = vars['X']
+    Y = vars['Y']
+    
+    
+loadvars()
--- Code/Zero_Objective_Func.py
+++ Code/Zero_Objective_Func.py
@@ -1,21 +1,8 @@
 import numpy as np
 from scipy.optimize import linprog
 from scipy.io import loadmat
-
-varss = loadmat('sp_ln_sp_data.mat')
-n = varss['n'][0][0]
-N = varss['N'][0][0]
-M = varss['M'][0][0]
-X = varss['X']
-Y = varss['Y']
-
+import Load_Data
 epsilon = 10 ** (-6)
-
-# _______________________________
-
-# Robime
-
-# -------------------------------
 
 # inequalities for x, where
 # -(x^T)a - b <= -1
@@ -32,55 +19,26 @@ a_y = a_y.transpose()
 # inequalities for x and y stacked
 a = np.vstack((a_x, a_y))
 
-# add 0s to the right
-
-# a = np.hstack((a, np.zeros((N + M, n))))
-
-# add extra inequalities we use for the absolute value
-
-# for x in 1, -1:
-#  inequality = np.hstack((x * np.eye(n), np.zeros((n, 1)), -np.eye(n)))
-#  a = np.vstack((a, inequality))
-
-print()
-# print(a.shape)
-# print(a)
-
-# exit()
-
-# right side for inequalities, -1 for x and y and 0 when were making the abs work
-# e = -np.ones(M + N)
-
-
 b = np.empty(100)
 b.fill(-1)
 print(b)
 
-# b = np.concatenate((b, np.zeros(2 * n)))
-
-# result.x is in the form [a_1, ..., a_n, b, t_1, ..., t_n]
-# and after the abs val transformation we're minimizing (1^T)t
-
-
 c = np.zeros(n + 1)
-# c = np.concatenate((c, np.ones(n)))
 
 m = ["highs-ds", "highs-ipm", "highs", "revised simplex", "interior-point", "simplex"]
 for u in m:
     print(u)
     result = linprog(c, A_ub=a, b_ub=b, method=u, bounds=(None, None))
-    # print(result)
+
 
     result = result.x
-    # result[abs(result) < epsilon] = 0
-    # print(result)
 
     a_result = result[:50]
     b_result = result[50]
-
-    print("a:")
-    print(a_result)
-    print("b:", b_result)
+    with open(u +".txt","w") as c:
+        c.write(f"a: {a_result}")
+        c.write("\n")
+        c.write(f"b: {b_result}")
 
     # for i, v in enumerate(a):
     #     out = result.x @ v
